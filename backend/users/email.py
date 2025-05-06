0414b59a0295e20b7e85e91b41e714c4609da570
zart227 <zart227@gmail.com>
2025-05-06 23:57:40 +0300

    feat(users): update email sending logic

--- backend/users/email.py
+++ backend/users/email.py
@@ -20,5 +20,16 @@ class OrderConfirmationEmail(email.BaseEmailMessage):
 
     def get_context_data(self):
         context = super().get_context_data()
+        # print(f"Context: {context}")
         context["order"] = self.context.get("order")
+        context["site_name"] = self.context.get("site_name")
+        context["domain"] = self.context.get("domain")
+        context["protocol"] = self.context.get("protocol")
+        context["delivery_address"] = self.context.get("delivery_address")
+        context["order_status_display"] = self.context.get("order_status_display")
+        # product_detail для каждого item, если есть
+        if context["order"]:
+            for item in context["order"].items.all():
+                if hasattr(item, 'product_detail'):
+                    item.product_detail = item.product_detail
         return context
